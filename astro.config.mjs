0aa93a47dc43d0ec406747389cc541db32cbd06f
danieltinois <danieltinois@gmail.com>
2026-03-27 14:41:00 -0300

    feat: add Vercel adapter for deployment and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
